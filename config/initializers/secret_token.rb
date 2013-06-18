a7037a5ae2fd20d0d01905dd109d99ccf9be72d1
Stanley Alston <stanleyalston@hotmail.com>
2013-06-18 23:32:37 +0800

    new trip app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wandrr::Application.config.secret_token = '6e9482325f12b223d8193be5f8c1ab354fbe7809e27bfd5c6ef30f21a684fb3a2918ac7e331754b870d37d11b3085c133da5a77d7f2b16ab730118f763545297'
